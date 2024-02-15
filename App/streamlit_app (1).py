484771c4b3abc74eef6d87d6b3bd90ee123e644d
Yumemi Kinsella <yumemi_0706@yahoo.co.jp>
2024-02-15 15:01:44 -0800

    change

--- App/streamlit_app (1).py
+++ App/streamlit_app (1).py
@@ -4,41 +4,59 @@ import pandas as pd
 import streamlit as st
 
 
-"""
-# Heart Disease Risk Prediction
+st.title("Welcome to Heart Disease Risk Prediction!!")
 
-## Fill out the Following Questions:
-"""
+st.header("Fill out the Following Questions:")
 
 st.write("Personal Detail Question")
 username = st.text_input("Enter your name")
 sex = st.radio("Select Gender:", ("Male", "Female"))
-age = st.selectbox("Select Age Group:", ["18-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54",
-                                        "55-59", "60-64", "65-69", "70-74", "75-79", "80 or older"])
-race = st.selectbox("Select Race:", ["White", "Hispanic", "Black", "Asian", "American Indian/Alaskan Native"])
-height = st.number_input("Enter your Height (in centimeters):", min_value=100.00, max_value=250.00, step=10.00)
-weight = st.number_input("Enter your Weight (in kilograms):", min_value=30.00, max_value=200.00, step=10.00)
+age = st.selectbox("Select Age Group:", 
+                   ["18-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54",
+                    "55-59", "60-64", "65-69", "70-74", "75-79", "80 or older"],
+                    placeholder="Select One")
+race = st.selectbox("Select Race:", ["White", "Hispanic", "Black", "Asian", "American Indian/Alaskan Native"],
+                    placeholder="Select One")
+height = st.number_input("Enter your Height (in centimeters):", min_value=100.00, max_value=250.00, 
+                         value=150.00, step=5.00)
+weight = st.number_input("Enter your Weight (in kilograms):", min_value=30.00, max_value=200.00, 
+                         value=50.00, step=5.00)
 
 
+st.text(" ")
+st.text(" ")
 st.write("Daily Activity Question")
-sleep = st.number_input("Enter Average Sleep Time:", min_value=0.0, max_value=24.0, step=1.0)
-activity = st.radio("During the past month, other than your regular job, did you participate in any physical activities or exercises such as running, calisthenics, golf, gardening, or walking for exercise?:", ("Yes", "No"))
-smoking = st.radio("Have you smoked at least 100 cigarettes in your entire life?:", ("Yes", "No"))
-drinking = st.radio("Do you drink a lot (adult men having more than 14 drinks per week or adult women having more than 7 drinks per week)?:", ("Yes", "No"))
+sleep = st.number_input("Enter Average Sleep Time:", min_value=0.0, max_value=24.0, 
+                        value=7.0, step=0.5)
+activity = st.radio("During the past month, other than your regular job, did you participate in any physical activities or exercises such as running, calisthenics, golf, gardening, or walking for exercise?:", 
+                    ("Yes", "No"), index=1)
+smoking = st.radio("Have you smoked at least 100 cigarettes in your entire life?:", 
+                   ("Yes", "No"), index=1)
+drinking = st.radio("Do you drink a lot (adult men having more than 14 drinks per week or adult women having more than 7 drinks per week)?:", 
+                    ("Yes", "No"), index=1)
 
+
+st.text(" ")
+st.text(" ")
 st.write("Health Status")
-genhealth = st.selectbox("Would you say that in general your health is:", ['Excellent', 'Very good', 'Good', 'Fair', 'Poor'])
+genhealth = st.selectbox("Would you say that in general your health is:", ['Excellent', 'Very good', 'Good', 'Fair', 'Poor'],
+                         index=2)
 physicalhealth = st.selectbox("During the past month, how many days did you have physical health problem, which includes physical illness and injury?:", range(0, 31))
 mentalhealth = st.selectbox("During the past month, how many days did you have mental health problem?:", range(0, 31))
-diffwalking = st.radio("Do you have serious difficulty walking or climbing stairs?:", ("Yes", "No"))
-asthma = st.radio("(Ever told) (you had) asthma?:", ("Yes", "No"))
+diffwalking = st.radio("Do you have serious difficulty walking or climbing stairs?:", 
+                       ("Yes", "No"), index=1)
+asthma = st.radio("(Ever told) (you had) asthma?:", ("Yes", "No"), index=1)
+
+st.text(" ")
+st.text(" ")
 button = st.button('Predict')
 
 bmi = weight / ((height/100)**2)
 
 
-# Creating a Machine Learning Model 
 
+
+# Creating a Machine Learning Model 
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
